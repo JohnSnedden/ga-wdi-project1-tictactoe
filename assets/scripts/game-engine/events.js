086ec8f8f7a3c6bc55fc7c70e7662ba9668f5eb5
JohnSnedden <john.snedden@libertymutual.com>
2017-09-28 07:08:10 -0400

    add initial components for showing player turn
    - add new div to html
    - add some basic styling for the new div
    - add new scripts folder for game-engine
    - add events.js file to game-engine folder
    - link events.js to index.js

---
+++ assets/scripts/game-engine/events.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// function for click on square
+// 1. check if square is open
+//     (flash square red if not open)
+// 2. change value of the sqaure to player symbol
+// 3. change turn indicator to other player
